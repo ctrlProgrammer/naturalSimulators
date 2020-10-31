989496410783d4227b05098a7cf1991ef2fefc22
Ctrlprogrammer <sebastianto1999@gmail.com>
2020-10-31 12:11:42 -0500

    Map building

--- src/core/main.js
+++ src/core/main.js
@@ -1,7 +1,53 @@
+class Map {
+  constructor(config, container) {
+    this.container = container;
+
+    this.config = {
+      ...config,
+      background: config.background ? config.background : "rgb(0,0,0)",
+      pixelSize: config.pixelSize ? config.pixelSize : 10,
+      size: config.size ? config.size : 10 * 20,
+    };
+
+    this.build();
+  }
+
+  build() {
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = this.config.size;
+    this.canvas.height = this.config.size;
+    this.ctx = this.canvas.getContext("2d");
+    this.container.appendChild(this.canvas);
+  }
+
+  background() {
+    this.ctx.beginPath();
+    this.ctx.fillStyle = this.config.background;
+    this.ctx.fillRect(0, 0, this.config.size, this.config.size);
+    this.ctx.closePath();
+  }
+}
+
 class Simulator {
   constructor() {
+    this.config = {
+      map: {
+        background: "rgb(0,0,0)",
+        pixelSize: 10,
+        size: 10 * 50,
+      },
+    };
+
     this.container = document.getElementById("ct-sim-container");
-    console.log(this.container);
+    this.map = new Map(this.config.map, this.container);
+
+    this.start();
+  }
+
+  start() {
+    this.simInterval = setInterval(() => {
+      this.map.background();
+    }, 500);
   }
 }
 
