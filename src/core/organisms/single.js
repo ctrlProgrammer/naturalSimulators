3d8a4a43a1d8f7e12adfaebedec294335e9e287e
Ctrlprogrammer <sebastianto1999@gmail.com>
2020-10-31 13:25:30 -0500

    Movement formulas

--- src/core/organisms/single.js
+++ src/core/organisms/single.js
@@ -16,20 +16,27 @@ export default class Single {
     this.props = {
       vel: 10,
       err: 10,
+      size: this.map.config.pixelSize,
     };
 
     this.status = {
       searching: true,
     };
 
+    this.mode = "dev";
+
     this.randomMove();
   }
 
   randomMove() {
     this.moveTo(
-      Math.random() * this.map.canvas.width,
-      Math.random() * this.map.canvas.height
+      Math.random() * (this.map.canvas.width - this.props.size),
+      Math.random() * (this.map.canvas.height - this.props.size)
     );
+  }
+
+  movementFormula(pointOne, pointTwo){
+    
   }
 
   moveTo(x, y) {
@@ -39,10 +46,10 @@ export default class Single {
 
   move() {
     if (
-      (this.pos.x <= this.to.x + this.props.err &&
-        this.pos.x >= this.to.x - this.props.err) &&
-      (this.pos.y <= this.to.y + this.props.err &&
-        this.pos.y >= this.to.y - this.props.err)
+      this.pos.x <= this.to.x + this.props.err &&
+      this.pos.x >= this.to.x - this.props.err &&
+      this.pos.y <= this.to.y + this.props.err &&
+      this.pos.y >= this.to.y - this.props.err
     ) {
       this.randomMove();
     }
@@ -59,7 +66,37 @@ export default class Single {
   print() {
     this.map.ctx.beginPath();
     this.map.ctx.fillStyle = "rgb(255,255,255)";
-    this.map.ctx.fillRect(this.pos.x, this.pos.y, 10, 10);
+    this.map.ctx.fillRect(
+      this.pos.x,
+      this.pos.y,
+      this.props.size,
+      this.props.size
+    );
     this.map.ctx.closePath();
+
+    if (this.mode === "dev") {
+      this.map.ctx.beginPath();
+      this.map.ctx.fillStyle = "rgb(0,0,255)";
+      this.map.ctx.fillRect(
+        this.to.x,
+        this.to.y,
+        this.props.size / 2,
+        this.props.size / 2
+      );
+      this.map.ctx.closePath();
+
+      this.map.ctx.beginPath();
+      this.map.ctx.strokeStyle = "rgb(255,0,0)";
+      this.map.ctx.moveTo(
+        this.pos.x + this.props.size / 2,
+        this.pos.y + this.props.size / 2
+      );
+      this.map.ctx.lineTo(
+        this.to.x + this.props.size / 4,
+        this.to.y + this.props.size / 4
+      );
+      this.map.ctx.stroke();
+      this.map.ctx.closePath();
+    }
   }
 }
