07d7183c2d1b9a643cf5e4e33d78bba51a5012e9
CtrlProgrammer <sebastianto1999@gmail.com>
2021-02-17 19:01:10 -0500

    Calc basis randoms

--- core/basis/helpers.ts
+++ core/basis/helpers.ts
@@ -2,6 +2,26 @@ import { Point } from "../types";
 
 export class RandomHelpers {
   static circleRandom(center: Point, radius: number) {}
-  static pointRandom(min: Point, max: Point) {}
-  static linearRandom(min: number, max: number) {}
+
+  static pointFloorRandom(min: Point, max: Point): Point {
+    return {
+      x: RandomHelpers.linearFloorRandom(min.x, max.x),
+      y: RandomHelpers.linearFloorRandom(min.y, max.y),
+    };
+  }
+
+  static pointRandom(min: Point, max: Point): Point {
+    return {
+      x: RandomHelpers.linearRandom(min.x, max.x),
+      y: RandomHelpers.linearRandom(min.y, max.y),
+    };
+  }
+
+  static linearFloorRandom(min: number, max: number): number {
+    return Math.floor(RandomHelpers.linearRandom(min, max));
+  }
+
+  static linearRandom(min: number, max: number): number {
+    return Math.random() * (max - min) + min;
+  }
 }
